9c68053b1bc27dfadb8056f5d8dc0eb915fd19e3
gfederica <effegiudice@gmail.com>
2021-04-28 00:15:41 +0200

    completato (aggiunto un po' di css)

--- js/script.js
+++ js/script.js
@@ -11,16 +11,16 @@ var prezzoBase = (0.21 * km);
 if (eta < 18) {
     var prezzoUnder = (prezzoBase - ((prezzoBase * 20) / 100)).toFixed(2);
     document.getElementById("sconto").innerHTML= "Sconto 20% per under 18";
-    document.getElementById("prezzo").innerHTML= prezzoUnder;
+    document.getElementById("prezzo").innerHTML= prezzoUnder + " €";
     //console.log(prezzoUnder);
 } else if (eta > 65) {
     var prezzoOver = (prezzoBase - ((prezzoBase * 40) / 100)).toFixed(2);
     document.getElementById("sconto").innerHTML= "Sconto 40% per over 65";
-    document.getElementById("prezzo").innerHTML= prezzoOver;
+    document.getElementById("prezzo").innerHTML= prezzoOver + " €";
     
 } else {
     var prezzoBase = prezzoBase.toFixed(2);
-    document.getElementById("prezzo").innerHTML= prezzoBase;
+    document.getElementById("prezzo").innerHTML= prezzoBase  + " €";
     document.getElementById("sconto").innerHTML= "Nessuno sconto applicato";
 }
 
